f372986df74240a025058b28e3409c66ac529b53
xFafouri <hamfaf.fafouri1@gmail.com>
2026-01-03 10:17:41 +0100

    add some css and finish the project

---
+++ weather-dashboard/node_modules/.vite/deps/react-dom.js
@@ -0,0 +1,4 @@
+import "./react-B5JTGcpc.js";
+import { t as require_react_dom } from "./react-dom-c5mxk89o.js";
+
+export default require_react_dom();
---
+++ weather-dashboard/node_modules/.vite/deps/react.js
@@ -0,0 +1,3 @@
+import { t as require_react } from "./react-B5JTGcpc.js";
+
+export default require_react();
